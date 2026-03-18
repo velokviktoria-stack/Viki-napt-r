994f6ffe778f4a7b1a8b6ecf8ff5347360344a40
velokviktoria-stack <velok.viktoria@gmail.com>
2026-03-18 19:01:04 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'IDE_JÖN_A_PROJECT_URL'
-const supabaseKey = 'IDE_JÖN_A_PUBLIKÁLHATÓ_KULCS'
+const supabaseUrl = 'https://txmypjrxogzxfwbxxumh.supabase.co'
+const supabaseKey = 'sb_publishable_-YLARZf1zMpYcJtQyvxYOw_gArZ4CLv'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
